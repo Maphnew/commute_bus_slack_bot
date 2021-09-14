d9b4276f63aab8231b78b062e2cd1135046c65a3
maphnew <zcm3118@gmail.com>
2021-09-14 19:36:17 +0900

    setup app for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const schedule = require('node-schedule')
 const app = express()
 const send = require('./slack/index')
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
