8283a036483bac97e838f73a4a1e00b57c1d9527
maphnew <zcm3118@gmail.com>
2021-09-26 00:43:59 +0900

    sheet

--- api/gsheet.js
+++ api/gsheet.js
@@ -1,14 +1,12 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-require('dotenv').config();
+const creds = require('./g-sheet-327101-133255eda106.json');
 
 const gsheet = async() => {
     // Initialize the sheet - doc ID is the long id in the sheets URL
     const doc = new GoogleSpreadsheet('1ukihBN0MaTnYircnWBRFi_TpvJSt570_Xid0YwX4yXM');
+    
     // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
-    await doc.useServiceAccountAuth({
-        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY,
-    }).catch(e => console.log(e));
+    await doc.useServiceAccountAuth(creds)
     
     await doc.loadInfo(); // loads document properties and worksheets
     console.log(doc.title);
@@ -23,4 +21,4 @@ const gsheet = async() => {
     await newSheet.delete();
 }
 
-gsheet();
+gsheet().catch(e => console.log('!!!!', e));;
