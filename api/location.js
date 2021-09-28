6d333f432385c54230044b43e4a7b84babab69b0
maphnew <zcm3118@gmail.com>
2021-09-28 22:16:41 +0900

    남은 시간 - 데이터 업데이트

--- api/location.js
+++ api/location.js
@@ -25,12 +25,12 @@ const location = async() => {
             return msg.msgBody.busLocationList.map(loc => {
                 const stationSeq = loc.stationSeq._text
                 const currentLocation = stations.response.msgBody.busRouteStationList[stationSeq-1]
-                return `\n${currentLocation.stationSeq}. ${currentLocation.stationName} - ${getDirection(stationSeq)}`
+                return `\n${currentLocation.stationSeq}. ${currentLocation.stationName} - ${getDirection(stationSeq)} ${currentLocation.minLeft ? currentLocation.minLeft + '분': ''}`
             })
         } else {
             const stationSeq = msg.msgBody.busLocationList.stationSeq._text
             const currentLocation = stations.response.msgBody.busRouteStationList[stationSeq-1]
-            return `${currentLocation.stationSeq}. ${currentLocation.stationName} - ${getDirection(stationSeq)}`
+            return `${currentLocation.stationSeq}. ${currentLocation.stationName} - ${getDirection(stationSeq)} ${currentLocation.minLeft ? currentLocation.minLeft + '분': ''}`
         }
         
         
